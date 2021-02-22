968065604b4f488981e3da74da5351977bfdade0
brittanymbui <brittymbui@gmail.com>
2021-02-22 09:31:38 -0800

    Adds paragraph to index-page

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,6 +2,7 @@ const IndexPage = () => {
     return (
         <div>
             <h1>Index Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ipsum!</p>
         </div>
     );
 }
